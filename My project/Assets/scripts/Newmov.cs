4e816973d73555dc2f817d98f86276daf608200a
Matheus Santos <matheussfelipe.santos@gmail.com>
2023-12-08 11:39:03 -0300

    Código de movimento da personagem comentado e alteração dos cenários

--- My project/Assets/scripts/Newmov.cs
+++ My project/Assets/scripts/Newmov.cs
@@ -18,6 +18,8 @@ public class Newmov : MonoBehaviour
 
     void Awake()
     {
+        /*Declarações de váriaveis que serão usadas no jogo 
+        e método para declarar os valores das variáveis do animator */
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         rb.interpolation = RigidbodyInterpolation2D.Interpolate; 
@@ -35,7 +37,7 @@ public class Newmov : MonoBehaviour
         AnimConditionsBool.Add("andfrente", false);
         AnimConditionsBool.Add("Parado", false);
     }
-
+    //Criação de setters SetAnimFloat e SetAnimBool para alterar os variaveis floats e bools respectivamente
     void SetAnimFloat(string condition, float value)
     {
         if (AnimConditionsFloat.ContainsKey(condition) && !Mathf.Approximately(AnimConditionsFloat[condition], value))
@@ -53,67 +55,71 @@ public class Newmov : MonoBehaviour
             animator.SetBool(condition, value);
         }
     }
-
+    //Método para calcular a velocidade
     void CalcularVelocidade()
     {
-        
+        //Passo 1 - Declarações de váriaveis para o cálculo de movimento, layers e RaycastHit2D
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector2 newMoveDirection = new Vector2(horizontal, vertical).normalized;
         Ray ray = new Ray(rb.position, moveDirection);
         LayerMask paredesLayerMask = LayerMask.GetMask("Default");
-        //bool mudouDirecao = (horizontal != 0 || vertical != 0) && (newMoveDirection.x == horizontal || newMoveDirection.y == vertical);
-        // Se houver uma colisão, ajustar o movimento
+        LayerMask portasLayerMask = LayerMask.GetMask("Portas");
+
+        // Declaração de variáves Raycast
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, smallDistance, paredesLayerMask);
+        RaycastHit2D porta = Physics2D.Raycast(ray.origin, ray.direction, smallDistance, portasLayerMask);
+        //Se houve colisão
         if (hit)
         {
              Debug.Log("Colidiu with: " + hit.collider.gameObject.name);
-             //newMoveDirection = Vector2.zero;
+             // Se o jogador estiver indo para uma direção oposta, a velocidade volta ao normal
              if (Vector2.Dot(moveDirection, newMoveDirection) <= 0)
-        {
-            // Se o jogador estiver indo para uma direção oposta, a velocidade volta ao normal
-            SPEED = 5f;
-        }
-        else
-        {
+            {
+                SPEED = 5f;
+            }
             // Caso contrário, a velocidade é definida como zero
-            SPEED = 0f;
+            else
+            {
+                SPEED = 0f;
+            }
         }
-        }
-            
+        //Se não houve colisão
         else{
             Debug.Log("Saiu");
            SPEED = 5f;
         }
+        //Quando se move a moveDirection vai obter os valores de Horizontal e Vertical normalizados
         if (newMoveDirection != Vector2.zero && newMoveDirection != moveDirection)
         {
             Debug.Log("Foi");
             moveDirection = newMoveDirection;
             
         }
+            //Declaração padrão das variaveis do animator
             SetAnimFloat("Vertical", Mathf.Abs(vertical));
             SetAnimFloat("Horizontal", Mathf.Abs(horizontal));
             SetAnimBool("Parado", false);
 
-            if (horizontal > 0)
+            if (horizontal > 0) //caso ande pro lado direito
             {
                 SetAnimBool("andlado", true);
                 SetAnimBool("andcostas", false);
                 SetAnimFloat("Speed", 5f);
             }
-            else if (horizontal < 0)
+            else if (horizontal < 0) //caso ande pro esquerdo
             {
                 SetAnimBool("andlado", true);
                 SetAnimBool("andcostas", false);
                 SetAnimFloat("Speed", 5f);
             }
-            else if(vertical > 0)
+            else if(vertical > 0) //caso ande pra cima
             {
                 SetAnimBool("andlado", false);
                 SetAnimBool("andcostas", true);
                 SetAnimFloat("Speed", 5f);
             }
-            else if(vertical < 0)
+            else if(vertical < 0) //caso ande pra baixo
             {
                 SetAnimBool("andlado", false);
                 SetAnimBool("andcostas", false);
@@ -127,7 +133,7 @@ public class Newmov : MonoBehaviour
             SetAnimFloat("Vertical", vertical);
            // newMoveDirection = Vector2.zero;
 
-            if (horizontal > 0)
+            if (horizontal > 0) //caso pare pro lado direito
             {
                 SetAnimBool("andlado", false);
                 SetAnimBool("andcostas", false);
@@ -136,7 +142,7 @@ public class Newmov : MonoBehaviour
                 SetAnimFloat("Speed", 0f);
                 SPEED = 0f;
             }
-            else if (horizontal < 0)
+            else if (horizontal < 0) //caso ande pro lado esquerdo
             {
                 SetAnimBool("andlado", false);
                 SetAnimBool("andcostas", false);
@@ -162,7 +168,7 @@ public class Newmov : MonoBehaviour
                 SetAnimBool("andfrente", false);
                 SetAnimBool("andfrente", false);
             }
-            else if (vertical < 0)
+            else if (vertical < 0) //caso pare pra cima
             {
                 
                 SetAnimBool("Parado",true);
@@ -171,7 +177,7 @@ public class Newmov : MonoBehaviour
                 SetAnimBool("andfrente", false);
                 SPEED = 0f;
             }
-            else
+            else //caso pare pra baixo
             {
                 SetAnimBool("Parado",true);
                 SetAnimFloat("Speed",0f);
@@ -179,16 +185,16 @@ public class Newmov : MonoBehaviour
                 SPEED = 0f;
             }
         }
-        if (moveDirection != Vector2.zero)
+        if (moveDirection != Vector2.zero) //Caso moveDirection seja diferente de nulo
         {
             rb.MovePosition(rb.position + moveDirection * Time.deltaTime * SPEED);
         }
-        else{
+        else{ //Se não continue nulo
             moveDirection = Vector2.zero;
         }
     }
 
-    void FixedUpdate()
+    void FixedUpdate() //Atualiza CalcularVelocidade de forma "exata"
     {
         CalcularVelocidade();
     }
